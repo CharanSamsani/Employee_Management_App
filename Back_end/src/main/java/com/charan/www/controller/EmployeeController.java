a568daade341a69420b11da3ad69bf784fa30216
Charan <charansamsani.official@gmail.com>
2024-09-27 22:22:00 +0530

    added DeleteEmployee method to EmployeeController.java

--- Back_end/src/main/java/com/charan/www/controller/EmployeeController.java
+++ Back_end/src/main/java/com/charan/www/controller/EmployeeController.java
@@ -1,16 +1,20 @@
 package com.charan.www.controller;
 
+package com.charan.www.controller;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.charan.www.model.Employee;
@@ -46,5 +50,12 @@ public class EmployeeController
 	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employee)
 	{
 		return employeeService.updateEmployee(id,employee);
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id)
+	{
+		return employeeService.deleteEmployee(id);
+		
 	}
 }
